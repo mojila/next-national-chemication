ac0755faa8b1bb428fa29170c9cff75c62d4c377
mojila <me.mojila@Mochs-MacBook.local>
2018-07-27 17:28:33 +0700

    dal

---
+++ .next/1.8ef9ac31e468f9b63f8d.hot-update.js
@@ -0,0 +1,2 @@
+webpackHotUpdate(1,[])
+//# sourceMappingURL=1.8ef9ac31e468f9b63f8d.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>Administrasi</title>
+                </Head>
+                <div className="mt-5">
+                    adaw
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Index
