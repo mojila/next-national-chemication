48152f7036775db291235ff40b6327b93cf0e156
mojila <me.mojila@Mochs-MacBook.local>
2018-07-27 16:48:56 +0700

    make it better

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const withCSS = require('@zeit/next-css')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = withCSS()
+module.exports = withCSS({
+    webpack: (config) => {
+        config.plugins = config.plugins.map((p) => {
+        if (p instanceof ExtractTextPlugin) {
+            p.options.disable = true
+        }
+        return p
+        })
+        
+        return config
+    },
+})
