9e648508001e3dea8496fb99496ef001e5c3a4f6
mojila <me.mojila@Mochs-MacBook.local>
2018-07-27 17:00:23 +0700

    adaw

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 const withCSS = require('@zeit/next-css')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = withCSS({
-    webpack: (config) => {
-        config.plugins = config.plugins.map((p) => {
-        if (p instanceof ExtractTextPlugin) {
-            p.options.disable = true
-        }
-        return p
-        })
-        
-        return config
-    },
-})
+module.exports = withCSS()
